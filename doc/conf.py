13909a9b22ca81edecd82ea92cd6eb2a3f5e9c7f
Tsukasa Aso <aso@nc-toyama.ac.jp>
2025-09-22 19:41:05 +0900

    Add extensions

--- doc/conf.py
+++ doc/conf.py
@@ -6,20 +6,43 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Example'
-copyright = 'workshop participant'
-author = 'workshop participant'
+project = 'TksASO'
+copyright = '2025-2030, Tsukasa Aso'
+author = 'Tsukasa Aso'
 release = '0.1'
 
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser']
+extensions = [
+    'myst_parser'
+]
+
+myst_enable_extensions = [
+    "amsmath",
+    "attrs_block",
+    "attrs_inline",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    # "linkify",
+    "replacements",
+    "smartquotes",
+    "strikethrough",
+    "substitution",
+    "tasklist",
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# -- Options for HTML output -------------------------------------------------
+today_fmt = "%Y-%m-%d %H:%M:%S"
+html_last_updated_fmt = "%Y-%m-%d %H:%M:%S"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
